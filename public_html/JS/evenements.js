64d80424a7b6818418718b0b50d9a1a2f1284190
khaoulani.sohayb@gmail.com <admin@dfef-PC>
2016-10-13 09:44:49 +0200

    ModulJavaScript

---
+++ public_html/JS/evenements.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function clicbouton(){
+    
+    var divTitre =   document.getElementById("titre");
+    divTitre.innerHTML = "you man!";
+}
+
